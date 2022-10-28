e7df3f0158dcf6716cd1f3bc7d8799f708ea0cd1
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2022-10-28 21:15:26 +0300

    Add files via upload

---
+++ arch-chroot.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
+
+hwclock --systohc
+
+echo "tr_TR.UTF-8 UTF-8" >> /etc/locale.gen
+echo "tr_TR ISO-8859-9" >> /etc/locale.gen
+
+echo 'LANG="tr_TR.UTF-8"' >> /etc/locale.conf
+
+echo "KEYMAP=trq" >> /etc/vconsole.conf
+
+echo "archlinux" >> /etc/hostname
+
+echo "127.0.0.1		localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+locale-gen
+
+pacman -S grub efibootmgr networkmanager
+
+pacman -S xorg-server xorg-apps i3 xfce4-terminal xfce4-panel xfce4-clipman-plugin lightdm lightdm-gtk-greeter 
+
+pacman -S pulseaudio pavucontrol intel-ucode base-devel git zip unzip udisks2 gvfs-mtp brightnessctl 
+
+systemctl enable NetworkManager
+systemctl enable lightdm
+
+mkinitcpio -P
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+grub-install /dev/sda --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo passwd pc parolası
+echo useradd -m bandolero
+echo passwd bandolero
+echo visudo
+echo exit
+echo umount -R /mnt     
+echo poweroff
