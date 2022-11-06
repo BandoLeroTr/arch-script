a893c30364cc1b3852ae5f7464ed4e6adc8a2c6e
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2022-11-07 00:39:53 +0300

    Add files via upload

---
+++ arch-chroot.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
+
+hwclock --systohc
+
+echo tr_TR.UTF-8 UTF-8 >> /etc/locale.gen
+
+echo LANG="tr_TR.UTF-8" >> /etc/locale.conf
+
+echo KEYMAP=trq >> /etc/vconsole.conf
+
+echo archlinux >> /etc/hostname
+
+echo 127.0.0.1		localhost >> /etc/hosts
+
+echo ::1		localhost >> /etc/hosts
+
+locale-gen
+
+pacman -Syu
+
+pacman -S grub efibootmgr networkmanager network-manager-applet linux-headers \
+      xorg-server i3 xfce4-terminal xfce4-clipman-plugin xfce4-taskmanager xfce4-screenshooter lightdm lightdm-gtk-greeter \
+      pacman -S pulseaudio pavucontrol intel-ucode base-devel git zip unzip udisks2 gvfs-mtp brightnessctl \
+      pacman -S picom nitrogen rofi thunar lxappearance vivaldi htop ristretto file-roller alsa-utils awesome-terminal-fonts zsh
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkinitcpio -P
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+grub-install /dev/sda --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#clear
+
+useradd -m bandolero
+
+echo ######## root passwd #######")
+passwd
+
+echo ######## user passwd ########
+passwd bandolero
+
+echo bandolero 'ALL=(ALL:ALL) ALL' >> /etc/sudoers
+
+#######################################################################################
+rm -r /home/bandolero/.config
+
+su bandolero
+
+python3 folder.py
+
+exit
+
+git clone https://github.com/BandoLeroTr/son.git
+cd son
+
+mv config .config
+
+cp -r .config /home/bandolero
+
+tar xf Tela-circle-manjaro.tar.gz
+unzip Qogir-dark.zip
+unzip Pax-GTK.zip
+
+cp -r Tela-circle-manjaro /usr/share/icons #folder icon
+cp -r Qogir-dark /usr/share/icons #cursor icon
+cp -r Pax-GTK /usr/share/themes #all color
+
+cp wallpapers.jpg ~/Pictures
+nitrogen --set-auto ~/Pictures/wallpapers.jpg
+
+mv vimrc .vimrc
+cp .vimrc ~/.vimrc
+cp xorg/00-keyboard.conf /etc/X11/xorg.conf.d/
+cp xorg/20-intel.conf /etc/X11/xorg.conf.d/
+
+systemctl enable NetworkManager
+systemctl enable lightdm
+clear
+
+#echo umount -R /mnt     
+#echo reboot
