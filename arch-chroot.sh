3ab134cbc370d72d3b18f331e24a9280e5c9c7c1
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2022-11-19 05:28:39 +0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
 ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
-
 hwclock --systohc
-
 echo tr_TR.UTF-8 UTF-8 >> /etc/locale.gen
-
 echo LANG="tr_TR.UTF-8" >> /etc/locale.conf
-
 echo KEYMAP=trq >> /etc/vconsole.conf
-
 echo archlinux >> /etc/hostname
-
 echo 127.0.0.1		localhost >> /etc/hosts
-
 echo ::1		localhost >> /etc/hosts
-
 locale-gen
 
-pacman -Syu
+echo [multilib] >> /etc/pacman.conf
+echo Include = /etc/pacman.d/mirrorlist >> /etc/pacman.conf
 
-pacman -S grub efibootmgr networkmanager network-manager-applet linux-headers \
-      xorg-server i3 xfce4-terminal xfce4-clipman-plugin xfce4-taskmanager xfce4-screenshooter lightdm lightdm-gtk-greeter \
-      pacman -S pulseaudio pavucontrol intel-ucode base-devel git zip unzip udisks2 gvfs-mtp brightnessctl \
-      pacman -S picom nitrogen rofi thunar lxappearance vivaldi htop ristretto file-roller alsa-utils awesome-terminal-fonts zsh
+pacman -Sy
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pacman -S grub efibootmgr networkmanager sudo
+
+pacman -S xorg i3 xfce4-terminal xfce4-clipman-plugin xfce4-taskmanager xfce4-screenshooter lightdm lightdm-gtk-greeter pulseaudio pavucontrol base-devel git zip unzip gvfs-mtp brightnessctl picom nitrogen rofi thunar thunar-archive-plugin lxappearance firefox htop ristretto file-roller alsa-utils awesome-terminal-fonts zsh steam steam-native-runtime
 
 mkinitcpio -P
 mkdir /boot/efi
@@ -39,30 +30,24 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 useradd -m bandolero
 
-echo ######## root passwd #######")
+echo ######## root passwd #######
 passwd
 
 echo ######## user passwd ########
 passwd bandolero
 
 echo bandolero 'ALL=(ALL:ALL) ALL' >> /etc/sudoers
-
 #######################################################################################
 rm -r /home/bandolero/.config
 
-su bandolero
-
-python3 folder.py
-
-exit
-
-git clone https://github.com/BandoLeroTr/son.git
-cd son
+git clone https://github.com/BandoLeroTr/arch-script.git
+cd arch-script
 
 mv config .config
 
 cp -r .config /home/bandolero
-
+#############################################
+cd themes
 tar xf Tela-circle-manjaro.tar.gz
 unzip Qogir-dark.zip
 unzip Pax-GTK.zip
@@ -70,15 +55,15 @@ unzip Pax-GTK.zip
 cp -r Tela-circle-manjaro /usr/share/icons #folder icon
 cp -r Qogir-dark /usr/share/icons #cursor icon
 cp -r Pax-GTK /usr/share/themes #all color
-
-cp wallpapers.jpg ~/Pictures
-nitrogen --set-auto ~/Pictures/wallpapers.jpg
+cd ..
+#############################################
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mv vimrc .vimrc
 cp .vimrc ~/.vimrc
 cp xorg/00-keyboard.conf /etc/X11/xorg.conf.d/
-cp xorg/20-intel.conf /etc/X11/xorg.conf.d/
-
+#######################################################################################
 systemctl enable NetworkManager
 systemctl enable lightdm
 clear
