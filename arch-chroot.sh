9493338ca57622e7e5c264ec4360a577df21b65d
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2022-11-20 23:14:29 +0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -19,6 +19,8 @@ pacman -S grub efibootmgr networkmanager sudo
 
 pacman -S xorg i3 xfce4-terminal xfce4-clipman-plugin xfce4-taskmanager xfce4-screenshooter lightdm lightdm-gtk-greeter pulseaudio pavucontrol base-devel git zip unzip gvfs-mtp brightnessctl picom nitrogen rofi thunar thunar-archive-plugin lxappearance firefox htop ristretto file-roller alsa-utils awesome-terminal-fonts zsh steam steam-native-runtime
 
+#pacman -S wine lutris
+
 mkinitcpio -P
 mkdir /boot/efi
 mount /dev/sda1 /boot/efi
@@ -38,8 +40,6 @@ passwd bandolero
 
 echo bandolero 'ALL=(ALL:ALL) ALL' >> /etc/sudoers
 #######################################################################################
-rm -r /home/bandolero/.config
-
 git clone https://github.com/BandoLeroTr/arch-script.git
 cd arch-script
 
@@ -62,11 +62,19 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 mv vimrc .vimrc
 cp .vimrc ~/.vimrc
+#######################################################################################
 cp xorg/00-keyboard.conf /etc/X11/xorg.conf.d/
+#######################################################################################
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+
+yay -S timeshift
+
 #######################################################################################
 systemctl enable NetworkManager
 systemctl enable lightdm
-clear
 
 #echo umount -R /mnt     
 #echo reboot
