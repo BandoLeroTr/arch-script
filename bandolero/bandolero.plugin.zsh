3e49a88419faa59e1118ab035b595ecad69c9924
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2022-12-15 01:38:32 +0300

    Add files via upload

---
+++ bandolero/bandolero.plugin.zsh
@@ -0,0 +1,14 @@
+alias s='sudo pacman -S'
+alias ss='sudo pacman -Ss'
+alias sy='sudo pacman -Sy'
+alias syu='sudo pacman -Syu'
+
+alias r='sudo pacman -Rns'
+
+alias y='yay -S'
+alias ys='yay -Ss'
+alias yr='yay -Rns'
+
+alias g='git clone'
+alias p='python3'
+alias t='sudo timeshift-gtk'
