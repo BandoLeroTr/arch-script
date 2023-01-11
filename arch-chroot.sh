e7b97583b082d1997e088d6663bd65e1610df3d4
Sarı Kola <90865536+BandoLeroTr@users.noreply.github.com>
2023-01-11 22:45:48 +0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -17,10 +17,10 @@ pacman -Sy
 
 pacman -S grub efibootmgr networkmanager sudo
 
-pacman -S xorg i3 xfce4-terminal xfce4-clipman-plugin xfce4-taskmanager xfce4-screenshooter lightdm lightdm-gtk-greeter pulseaudio pavucontrol \
+pacman -S xorg i3 xfce4-terminal lightdm lightdm-gtk-greeter pulseaudio pavucontrol \
     base-devel git zip unzip gvfs-mtp brightnessctl picom nitrogen rofi thunar thunar-archive-plugin lxappearance firefox ristretto file-roller \
-    alsa-utils awesome-terminal-fonts zsh steam ttf-liberation nodejs yarn python-pip scrot ffmpegthumbnailer \
-    weston gnu-free-fonts ttf-hack-nerd    
+    alsa-utils awesome-terminal-fonts zsh steam ttf-liberation nodejs yarn python-pip scrot ffmpegthumbnailer xfce4-clipman-plugin xfce4-taskmanager 
+    xfce4-screenshooter weston gnu-free-fonts ttf-hack-nerd    
 
 #modprobe vboxdrv
 
