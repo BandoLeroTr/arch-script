5488e895afac2bb1712c22201699fdd44dbe20ea
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2023-01-11 23:13:35 +0300

    Update arch-iso.sh

--- arch-iso.sh
+++ arch-iso.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# 1 - Kuruluma başlamadan önce cgdisk /dev/sda ile disk bölümleme işlemini yapın. 
-# 2 - chmod +x arch-iso.sh
-# 3 - ./arch.sh
+# 1 - Kuruluma başlamadan önce cfdisk /dev/sda ile disk bölümleme işlemini yapın. 
+# 2 - curl https://bandolerotr.github.io/arch-script/arch-iso.sh -o a.sh
+# 3 - chmod +x arch-iso.sh
+# 4 - ./arch.sh
 
 loadkeys trq
 timedatectl set-ntp true
 
-mkfs.fat -F 32 /dev/sda1
+#mkfs.fat -F 32 /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
 mkfs.ext4 /dev/sda3
