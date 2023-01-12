eb377ccbb0d2e6fe29fdfdc0d00282add83a90d4
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2023-01-12 14:57:52 +0300

    Update arch-iso.sh

--- arch-iso.sh
+++ arch-iso.sh
@@ -10,24 +10,21 @@ timedatectl set-ntp true
 
 mkfs.fat -F 32 /dev/sda1
 mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.ext4 /dev/sda6
+mkfs.ext4 /dev/sda3
 
 mount /dev/sda3 /mnt
 mkdir /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
+swapon /dev/sda2
 
 #pacman-key --init
 #pacman-key --populate archlinux
 
 pacman -Sy
 
-pacstrap /mnt archlinux-keyring base linux-zen linux-firmware neovim \
-    grub efibootmgr networkmanager sudo \
-    xorg i3 xfce4-terminal lightdm lightdm-gtk-greeter pulseaudio pavucontrol \
-    base-devel git zip unzip gvfs-mtp brightnessctl picom nitrogen rofi thunar thunar-archive-plugin lxappearance firefox ristretto file-roller \
-    alsa-utils awesome-terminal-fonts zsh steam ttf-liberation nodejs yarn python-pip scrot ffmpegthumbnailer xfce4-clipman-plugin xfce4-taskmanager \
-    xfce4-screenshooter weston gnu-free-fonts ttf-hack-nerd ntfs-3g os-prober
+pacman -S archlinux-keyring git
+
+pacstrap /mnt base linux-zen linux-firmware neovim 
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
