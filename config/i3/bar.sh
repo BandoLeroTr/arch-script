b1064590360d3c81ea87ef75e90a189c1933e431
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2023-05-17 23:54:14 +0300

    Add files via upload

---
+++ config/i3/bar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+	font pango: Noto Sans Regular 10
+	status_command i3blocks -c ~/.config/i3/i3blocks.conf
+	position bottom
+
+
+
+	tray_output primary
+	tray_padding 0
+
+
+
+
+strip_workspace_numbers yes
+
